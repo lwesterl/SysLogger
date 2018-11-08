e70de8de207046b3f9ad8cc0d692199b5f43eaf6
lwesterl <lauri.v.westerholm@gmail.com>
2018-11-08 18:58:20 +0200

    Added prints, parent processes now waits for childs to finish, args check for valgrind test and SIGINT blocking

--- stress_test.c
+++ stress_test.c
@@ -13,24 +13,50 @@
  *    Each thread tries to write a message to SysLogger using syslogger function
  *
  *    FORKS_TARGET defines amount of the forks
+ *
+ *    There is a special call for this function when it doesn't check whether
+ *    SysLogger is running or not. It's should be only used through Makefile,
+ *    make test_stress_test, and then it is user responsibility to first call
+ *    make test_memory. Otherwise multiple fifos (FORKS_TARGET * THREADS_TARGET)
+ *    are generated to /tmp
  */
 
- int main(void)
+ int main(int args, char* argv[])
  {
-   printf("Stress test started\n");
+   printf("SysLogger stress test started\n");
+   printf("If everything goes well, /var/tmp/SysLogger.log should contain log entries:\n");
+   printf("Time Stress_tester pid: Stress tester process number, x, thread number, y\n");
+   printf("\nIf stress test fails, SysLogger failed prints are shown\n");
 
-   /* Check the pid file exists, pid file location is defined in logger_header.h
-      but including the whole header seems a bit excessive                    */
 
-   int fd = open("/tmp/SysLogger.pid", O_RDONLY);
-   if (fd < 0) {
-     printf("SysLogger isn't running at the moment\n");
-     return 0;
+   if (args == 2 && strcmp(argv[1], "mem_test") == 0) {
+     /* make mem_test and make stress_tester started (both must be on at this point) */
+     printf("Skip SysLogger run check\n");
    }
-   /* Opening the pid file is used just as an indicator that SysLogger is running */
-   close(fd);
-   pid_t pid;
+   else {
+     /* Check the pid file exists, pid file location is defined in logger_header.h
+        but including the whole header seems a bit excessive                    */
 
+     int fd = open("/tmp/SysLogger.pid", O_RDONLY);
+     if (fd < 0) {
+       printf("SysLogger isn't running at the moment\n");
+       return 0;
+     }
+     /* Opening the pid file is used just as an indicator that SysLogger is running */
+     close(fd);
+   }
+
+   /* Block SIGINT to avoid possible memory leaks caused by user interrupt */
+   sigset_t block_set;
+   sigemptyset(&block_set);
+   sigaddset(&block_set, SIGINT);
+   if (pthread_sigmask(SIG_BLOCK, &block_set, NULL) != 0) {
+     perror("Error [ pthread_sigmask ] ");
+     return -1;
+   }
+
+   pid_t pid;
+   
    for (int i = 0; i < FORKS_TARGET; i ++)
    {
      /* Fork */
@@ -44,6 +70,12 @@
        return -1;
      }
    }
+
+   /* Now wait for all the childs to exit */
+   int status = 0;
+   pid_t wait_pid;
+
+   while ((wait_pid = wait(&status)) > 0);
    /* Return from parent */
    return 0;
  }
--- stress_test.h
+++ stress_test.h
@@ -15,6 +15,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <pthread.h>
 #include "syslogger.h"   /* syslogger defined there */
